4b58099ca3d9503c395e0b319b27af6f4d648d7a
artin <artin@debian.artin>
2026-06-05 00:07:25 +0330

    a new change

--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import base
+from django.http import Http404,HttpResponse
 # Create your views here.
 def home(request):
     return render(request,'blog/index.html')
@@ -7,3 +8,14 @@ def home(request):
 def blog(request):
     context = {'a':base.base}
     return render(request,'blog/blog.html',context)
+
+def det_blog(request,name):
+    context = {'i' : 0}
+    for i in base.base:
+        print(type(name),type(i['id']))
+        if name == i['id']:
+            context['i'] = 0
+    
+    if context['i'] != 0:
+        return render(request,'blog/blog_det.html',context)
+    raise Http404()
