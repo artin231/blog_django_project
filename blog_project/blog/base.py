3f63ba7fe0c99805a58351852226d2fbc420809e
artin <artin@debian.artin>
2026-06-04 21:04:15 +0330

    adding a static database file

---
+++ blog_project/blog/base.py
@@ -0,0 +1,7 @@
+base = [
+    {'title' : 'python or js', 'description' : '''
+Comparing Python vs JavaScript performance, JavaScript is a clear winner because it runs faster than Python in web development. JS is preferable for high-performance and real-time interactions. However, Python is also an excellent choice for CPU-intensive tasks.
+'''},
+    {'title' : 'python or java', 'description' : '''
+In short: Choose Python for faster development, simpler syntax, and use cases like AI, data science, and prototyping. Choose Java for scalability, performance, and large enterprise systems with complex architectures. Python is easier to learn and quicker to iterate with, making it ideal for startups and smaller teams.'''}
+]
