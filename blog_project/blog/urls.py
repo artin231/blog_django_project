1b9036ac11534b88054c3573261b8f1ebde82641
artin <artin@debian.artin>
2026-06-05 00:44:01 +0330

    adding the about_us page in the site

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('' , views.home,name='home'),
     path('posts/' , views.blog , name='blog'),
-    path('posts/<name>',views.det_blog , name='det_blog')
+    path('posts/<name>',views.det_blog , name='det_blog'),
+    path('about/',views.about , name='about')
 
 ]
