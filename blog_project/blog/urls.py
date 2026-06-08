d9902d220d8e391a4d7a7c950e065a0985679a6e
artin231 <leiartrezjah@gmail.com>
2026-06-08 13:05:57 +0330

    a change

--- blog_project/blog/urls.py
+++ blog_project/blog/urls.py
@@ -8,8 +8,5 @@ urlpatterns = [
     path('posts/<slug:name>',views.det_blog , name='det_blog'),
     path('about/',views.about , name='about'),
     path('make_blog/',views.make_blog , name='make'),
-    path('posts/' , views.blog , name='blog'),
-    path('posts/<name>',views.det_blog , name='det_blog'),
-    path('about/',views.about , name='about')
 
 ]
--- blog_project/blog/views.py
+++ blog_project/blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import Http404,HttpResponse
 from .models import blog
 
-from . import base
 from django.http import Http404,HttpResponse
 # Create your views here.
 def home(request):
@@ -26,21 +25,3 @@ def about(request):
 
 def make_blog(request):
     return render(request,'blog/form.html')
-
-def blog(request):
-    context = {'a':base.base}
-    return render(request,'blog/blog.html',context)
-
-def det_blog(request,name):
-    context = {'i' : 0}
-    for i in base.base:
-        print(type(name),type(i['id']))
-        if name == i['id']:
-            context['i'] = i
-    
-    if context['i'] != 0:
-        return render(request,'blog/blog_det.html',context)
-    raise Http404()
-
-def about(request):
-    return render(request,'blog/about_us.html')
